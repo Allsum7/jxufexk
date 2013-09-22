2c8a8b423cbd2d77ccc7c6c4afbf19f4e7caf33f
Allsum7 <lee.huan92@gmail.com>
2013-09-22 23:16:40 +0800

    修复校内校外设别错误的问题，并美化一些对话框，更新到0.2.3

--- chrome/js/login.js
+++ chrome/js/login.js
@@ -1,90 +1,119 @@
 var channels = chrome.extension.getBackgroundPage().Gobal_channels; //获取得到的通道列表
-var isInSchool =  chrome.extension.getBackgroundPage().Gobal_isInSchool; //获取是否在校内
-var channel =""; //当前通道
+var isInSchool = chrome.extension.getBackgroundPage().Gobal_isInSchool; //获取是否在校内
+var channel = ""; //当前通道
 var isAutoIdentify = true;
 var isAutoSelectCourse = false;
 var autoSelectCourseForm = {};
-var isFindAChannel  = false;
+var isStopFinding = false;
+var currentJ = 0;
 
-    //ajax 全局事件
-	$(document).ajaxStart(function(){
-		$("#Loadingimg").show();
-	})
-	.ajaxSuccess(function(evt, request, settings){
-		$("#Loadingimg").hide();
-	})
-    .ajaxError(function(event,request, settings){
-		$("#Loadingimg").html("啊哦！网络请求出错!可能是服务器崩溃咯！");
-	});
-
-
-function myAjax(method, url, isAsync, parameters, channelI,callback) {
-
-    $.ajax({
-        type: method,
-        url: url,
-        data: parameters,
-        async: isAsync,  //同步或异步
-        success: function (data) {
-            callback(channelI,data);
+function myAjax(method, url, isAsync, parameters, channelI, callback)
+{
+    
+    $.ajax(
+    {
+        type : method,
+        url : url,
+        data : parameters,
+        async : isAsync, //同步或异步
+        success : function (data)
+        {
+            callback(channelI, data);
         }
-    });
+    }
+    );
 }
 
-function findAChannel() {      //随机选择一个通道,这里可以优化，但是考虑到服务器的承受能力不建议那么做
+/*
+function findAGoodChannel()  //该方法对服务器压力过大不建议使用，随机获取已经很好了
+{
+    var url = 'loginCode.jsp';
+    for (var i = 0; i < channels.length; i++)
+    {
+        if (isStopFinding)
+        {
+            //$("#signBtn").disabled = false;
+            return;
+        }
+        currentJ = i;
+        channel = channels[i];
+        myAjax("get", channel + url, false, "", currentJ + 1, forAChannel);
+    }
+    if (!isStopFinding)
+    { //如果一遍下来全满了，就随机找一个通道
+        $("#myMesgBox").html("唉！所有通道都满啦！(@^_^@)所以帮你随机选择了一个，或者你可以再试试？");
+        $("#myMesgBox").show();
+        randomAChannel();
+    }
+}*/
+
+function randomAChannel()
+{ //随机选择一个通道
     var url = 'loginCode.jsp';
     var randomI = parseInt(Math.random() * (channels.length - 1));
     currentJ = randomI;
     channel = channels[randomI];
     //alert("当前使用通道:" + channel);
-    myAjax("post",channel+url,true,"",currentJ+1,forAChannel);
+    myAjax("get", channel + url, true, "", currentJ + 1, forAChannel); //currentJ+1  表示显示的通道序号 从1开始
     $("#signBtn").disabled = false;
 }
-function forAChannel(channelI,data) {
 
-    if (data == "") {
+function forAChannel(channelI, data)
+{
+    
+    if (data == "")
+    { //表示这个通道没满！
         channel = channels[currentJ];
-        isFindAChannel = true;
-        $("#authImg").html("<img id='loginImg' src='" + channel + 
-		"loginSign.jsp' border=0>");
-                                                       
+        isStopFinding = true;
+        $("#myMesgBox").html(" (*^__^*)恭喜你！RP不错！一下就找到了一个没满的通道，赶紧填验证码");
+        $("#myMesgBox").show();
+        document.getElementById("signBtn").disabled = true;
+        $("#authImg").html("<img id='loginImg' src='" + channel +
+            "loginSign.jsp' border=0>");
+        
     }
-    else {
-        $("#authImg").html("<img id='loginImg' src='" + channel + 
-		"loginSign.jsp' border=0><INPUT id='changeCode' class='btn btn-warning' type=button value=我变！>");
-        $("#codePanel").html("<tr><td colspan='2'><span style='color:red'>随机获取到了通道"+channelI+"但人数已满!请重新选择通道!</span></td></tr>");
+    else
+    {
+        document.getElementById("signBtn").disabled = false;
+        $("#authImg").html("<img id='loginImg' src='" + channel +
+            "loginSign.jsp' border=0>");
+        $("#codePanel").html("<tr><td colspan='2'><span style='font-size:12px;color:#cc0000'>随机获取到了通道" + channelI + ",但这个通道人数已满!建议重新选择通道!</span></td></tr>");
     }
-
+    
 }
-function searchFruit() {
-
+function searchFruit()
+{
+    
     var url = 'cert.jsp';
     var form = $("#loginForm").serialize();
-    myAjax("post",channel+url,true,form,currentJ+1,showResponse);
-
+    myAjax("post", channel + url, true, form, currentJ + 1, showResponse);
+    
 }
 
-function showResponse(channelI,data) {
-
-
-    if (isAutoIdentify) {
-        if (data.match(loginForm.stuname.value) != null) {
+function showResponse(channelI, data)
+{
+    
+    if (isAutoIdentify)
+    {
+        if (data.match(loginForm.stuname.value) != null)
+        {
             //alert("认证成功！");
-			
+            
             checkUserInfo();
             return;
         }
-        var ttt = data.replace(/<\/?.+?>/g, "");    //去掉得到的html标签
+        var ttt = data.replace(/<\/?.+?>/g, ""); //去掉得到的html标签
         ttt = ttt.replace(/&nbsp;/g, "");
         $('#tips').html(ttt);
         $("#tips").show();
-        $("#authImg").html("<img id='loginImg' src='"+channel+"loginSign.jsp?tempTime='" + Math.random() + " border=0>");
-		loginForm.loginButton.disabled=false;
+        $("#authImg").html("<img id='loginImg' src='" + channel + "loginSign.jsp?tempTime='" + Math.random() + " border=0>");
+        loginForm.loginButton.disabled = false;
     }
-    else {
+    else
+    {
         $('#tips').html(data);
         $("#tips").show();
-		loginForm.loginButton.disabled=false;
+        loginForm.loginButton.disabled = false;
         //$("#authImg").html("<img id='loginImg' src='loginSign.jsp?tempTime='" + Math.random() + " border=0>");
     }
 }
@@ -93,210 +122,320 @@ function showResponse(channelI,data) {
 
 
 //2.ajax学生信息认证函数---------------------------------------------------------
-function checkUserInfo() {
+function checkUserInfo()
+{
     var url = 'confirmStudentInfo.jsp';
-    myAjax("post",channel+url,true,"",currentJ+1,showResponse2);
-
+    myAjax("post", channel + url, true, "", currentJ + 1, showResponse2);
+    
 }
-function showResponse2(channelI,data) {
-    if (isAutoIdentify) {
-        if (data.match(loginForm.stuname.value) != null) {
+function showResponse2(channelI, data)
+{
+    if (isAutoIdentify)
+    {
+        if (data.match(loginForm.stuname.value) != null)
+        {
             //alert("认证成功！");
             checkPermit();
             return;
         }
-        var ttt = data.replace(/<\/?.+?>/g, "");    //去掉得到的html标签
+        var ttt = data.replace(/<\/?.+?>/g, ""); //去掉得到的html标签
         ttt = ttt.replace(/&nbsp;/g, "");
         $('#tips').html(ttt);
         $("#tips").show();
-		loginForm.loginButton.disabled=false;
+        loginForm.loginButton.disabled = false;
     }
-    else {
+    else
+    {
         $('#tips').html(data);
         $("#tips").show();
     }
-
+    
 }
 //2.结束-------------------------------------------------------------------------
 
 
 //3.ajax学生是否有权利选课-------------------------------------------------------
-function checkPermit() {
-
+function checkPermit()
+{
+    
     var url = 'permission.jsp';
-    myAjax("post",channel+url,true,"",currentJ+1,showResponse3);
-
+    myAjax("post", channel + url, true, "", currentJ + 1, showResponse3);
+    
 }
-function showResponse3(channelI,data) {
- 
-    if (data.match("所有条件均符合选课条件") != null) {     //可改
+function showResponse3(channelI, data)
+{
+    
+    if (data.match("所有条件均符合选课条件") != null)
+    { //可改
         var url = "./studentSelectSubject.htm";
         chrome.extension.getBackgroundPage().Gobal_currentChannel = channel;
-        if (isAutoSelectCourse) {
+        if (isAutoSelectCourse)
+        {
             chrome.extension.getBackgroundPage().Gobal_isAutoSelectCourse = true;
-
+            
             chrome.extension.getBackgroundPage().Gobal_courseForm = autoSelectCourseForm;
         }
-		
-		chrome.extension.sendRequest({loginIsOk: "yes"}, function(response) {
-                console.log(response.farewell);
-	    });
+        
+        chrome.extension.sendRequest(
+        {
+            loginIsOk : "yes"
+        }, function (response)
+        {
+            console.log(response.farewell);
+        }
+        );
         //window.location.href = url;
     }
-	else {
-	    var ttt = data.replace(/<\/?.+?>/g, "");    //去掉得到的html标签
+    else
+    {
+        var ttt = data.replace(/<\/?.+?>/g, ""); //去掉得到的html标签
         ttt = ttt.replace(/&nbsp;/g, "");
         $('#tips').html(ttt);
         $("#tips").show();
-		loginForm.loginButton.disabled=false;
-	}
+        loginForm.loginButton.disabled = false;
+    }
 }
 
 //3.结束-------------------------------------------------------------------------
 
 
-function changeImage() {
+function changeImage()
+{
     document.getElementById("loginImg").src = channel + "loginSign.jsp?temptime=" + Math.random();
 }
 
-
-function changeBox() {
-
-    $("#mesPanel").css("display","none");
-
+function changeBox()
+{
+    
+    $("#mesPanel").css("display", "none");
+    
     loginForm.loginButton.disabled = false;
     $('#codePanel').html("");
     var url = 'loginSign.jsp';
-   
+    
     userName = loginForm.username.value;
     pwd = loginForm.password.value;
-    if (loginForm.username.value.length != 10 || loginForm.password.value.length == 0) {
+    if (loginForm.username.value.length != 10 || loginForm.password.value.length == 0)
+    {
         alert("亲，你的卡号应该为10位，密码不能为空哦");
         return false;
-    } else if (loginForm.username.value.indexOf(loginForm.password.value) != -1) {
+    }
+    else if (loginForm.username.value.indexOf(loginForm.password.value) != -1)
+    {
         alert("亲，你的填写的密码为弱口令，不能登录哦");
         return false;
-    } else if (isAutoIdentify&&loginForm.stuname.value.length < 7) {
+    }
+    else if (isAutoIdentify && loginForm.stuname.value.length < 7)
+    {
         alert("亲，你选择了自动认证模式，所以你填写的学号不能为空或少于7位哦！再检查下！");
         return false;
-    } else {
-        localStorage.setItem("ecardNum",loginForm.username.value) ;
-        localStorage.setItem("ecardPwd",loginForm.password.value) ;
-        localStorage.setItem("stuNum",loginForm.stuname.value) ;
-        $('#signBtn').disabled = true;
-        findAChannel();
-
+    }
+    else
+    {
+        localStorage.setItem("ecardNum", loginForm.username.value);
+        localStorage.setItem("ecardPwd", loginForm.password.value);
+        localStorage.setItem("stuNum", loginForm.stuname.value);
+        document.getElementById("signBtn").disabled = true;
+        randomAChannel();
+        
     }
 }
 
-function formatCourses(str) {
-
+function formatCourses(str)
+{
+    
     var args = str.split("&");
-    for (var i = 0; i < args.length; i++) {
+    for (var i = 0; i < args.length; i++)
+    {
         var arg = args[i].split("=");
         this[arg[0]] = arg[1];
     }
 }
 
-function checkForm() {
-
-	if(isAutoSelectCourse){
-		var autoSelectCourseFormStr = $("#autoSelectCourseForm").serialize();  //先序列化
-		//alert(autoSelectCourseFormStr);
-		autoSelectCourseForm  = new formatCourses(autoSelectCourseFormStr);  //再格式化成对象数组
-		var flagForAllEmpty = false;
-		for (var i = 1; i <= 5; i++) {     //保存自动选课表
-			if (autoSelectCourseForm["courseId" + i] != ""){
-				flagForAllEmpty = true; //表示不全为空
-				if(autoSelectCourseForm["classId" + i]=="") {
-					alert(autoSelectCourseForm["courseId" + i]+"的班号为空哦！请重新填写！");
-					return;
-				}
-			}
-		}
-		if(!flagForAllEmpty){
-		   alert("温馨提醒:由于发现你开启了自动选课模式，但是提交的预选课表是空的，不建议你酱紫！这样会使你只有五分钟的选课时间！如果不想自动选课，请点击预选课表右上角那个小叉叉哦！");
-		   return;
-		}
-	}
+function checkForm()
+{
+    
+    if (isAutoSelectCourse)
+    {
+        var autoSelectCourseFormStr = $("#autoSelectCourseForm").serialize(); //先序列化
+        //alert(autoSelectCourseFormStr);
+        autoSelectCourseForm = new formatCourses(autoSelectCourseFormStr); //再格式化成对象数组
+        var flagForAllEmpty = false;
+        for (var i = 1; i <= 5; i++)
+        { //保存自动选课表
+            if (autoSelectCourseForm["courseId" + i] != "")
+            {
+                flagForAllEmpty = true; //表示不全为空
+                if (autoSelectCourseForm["classId" + i] == "")
+                {
+                    alert(autoSelectCourseForm["courseId" + i] + "的班号为空哦！请重新填写！");
+                    return;
+                }
+            }
+        }
+        if (!flagForAllEmpty)
+        {
+            alert("温馨提醒:由于发现你开启了自动选课模式，但是提交的预选课表是空的，不建议你酱紫！这样会使你只有五分钟的选课时间！如果不想自动选课，请点击预选课表右上角那个小叉叉哦！");
+            return;
+        }
+    }
     //alert(autoSelectCourseForm["courseId1"]);
-
-    if (loginForm.username.value.length != 10 || loginForm.password.value.length == 0) {
+    
+    if (loginForm.username.value.length != 10 || loginForm.password.value.length == 0)
+    {
         alert("亲，你的卡号应该为10位，密码不能为空哦");
         return false;
-    } else if (loginForm.username.value.indexOf(loginForm.password.value) != -1) {
+    }
+    else if (loginForm.username.value.indexOf(loginForm.password.value) != -1)
+    {
         alert("亲，你填写的密码为弱口令，不能登录哦");
         return false;
-    } else if (loginForm.signImg.value.length == 0) {
+    }
+    else if (loginForm.signImg.value.length == 0)
+    {
         alert("亲，校验码不得为空哦");
         return false;
-    } else if (isAutoIdentify&&loginForm.stuname.value.length < 7) {
+    }
+    else if (isAutoIdentify && loginForm.stuname.value.length < 7)
+    {
         alert("亲，你选择了自动认证模式，所以你填写的学号不能为空或少于7位哦！再检查下！");
         return false;
-    } else {
+    }
+    else
+    {
         $('#mesPanel').show();
+		localStorage.setItem("ecardNum", loginForm.username.value);
+        localStorage.setItem("ecardPwd", loginForm.password.value);
+        localStorage.setItem("stuNum", loginForm.stuname.value);
         loginForm.loginButton.disabled = true;
-        $('#tips').css("display","none");
+        $('#tips').css("display", "none");
         searchFruit();
     }
 }
 
-document.onkeydown = function () {  //监听键盘回车事件
-    if (event.keyCode == 13) {
-        checkForm();
-    }
-}
-
-$("#openAuto").bind("click",function(){
-    alert("选中了自动选课模式，需要填写左下角那张预选课表，而且需要注意的是，为了公平起见，自动选课模式，进去只有五分钟的选课时间" +
-          "，时间到了会自动退出！！需要重新登陆哦！详细问题见[常见问题及回答]");
-	  isAutoSelectCourse = true;
-	  $("#loginPanel").attr("class","span5");
-	  $("#autoSelect").attr("class","span4");
-	  $("#showText").attr("class"," ");
-	  $("#showText").hide();
-	  $("#autoSelect").fadeIn("slow");
-	  //$("#autoSelect").show();
-    
-});
-
-$("#closeForm").bind("click",function(){
-      isAutoSelectCourse = false;
-      $("#loginPanel").attr("class","span9");
-      $("#autoSelect").hide();
-	  
-});
-
-$("#signBtn").bind("click",function(){
-    changeBox();
-});
-
-$("#closeTips").bind("click",function(){
-    $("#myTips").fadeOut("slow");
-});
-
-$("#closeMesPanel").bind("click",function(){
-    $("#mesPanel").fadeOut("fast");
-});
-
-$("#loginBtn").bind("click",function(){
-    checkForm();
-});
-$("#changeCode").bind("click",function(){
-   changeImage();
-});
-
-$("#signImg").keyup(function () {
-		if (document.getElementById("signImg").value.length == 4) checkForm();
-});
-function  readLocalStore(){
+function readLocalStore()
+{
     var ecardNum = localStorage.getItem("ecardNum");
-    var ecardPwd =  localStorage.getItem("ecardPwd");
+    var ecardPwd = localStorage.getItem("ecardPwd");
     var stuNum = localStorage.getItem("stuNum");
     $("#username").val(ecardNum);
     $("#password").val(ecardPwd);
     $("#stuname").val(stuNum);
+    if (ecardNum != null && ecardPwd != null && stuNum != null)
+    {
+        randomAChannel(); // 如果信息已经有了就直接随机一个通道
+        $("#signImg").focus();
+    }
+    else
+    {
+        $("#username").focus();
+    }
 }
 
-readLocalStore();
-
+window.onload = function forOnLoad() //载入页面就执行
+{
+    
+    //ajax 全局事件
+    $(document).ajaxStart(function ()
+    {
+        $("#Loadingimg").show();
+    }
+    )
+    .ajaxSuccess(function (evt, request, settings)
+    {
+        $("#Loadingimg").hide();
+    }
+    )
+    .ajaxError(function (event, request, settings)
+    {
+        $("#Loadingimg").html("啊哦！网络请求出错!可能是服务器崩溃咯！");
+    }
+    );
+    
+    document.onkeydown = function ()
+    { //监听键盘回车事件
+        if (event.keyCode == 13)
+        {
+            checkForm();
+        }
+    }
+    
+    $("#openAuto").bind("click", function () //自动选课确认
+    {
+        $("#showTextForAuto").html("<p>你选中了自动选课模式，需要填写左下角那张预选课表</p><p><font style='color:red'>而且需要注意的是，为了公平起见，自动选课模式，进去只有五分钟的选课时间</font></p>" +
+            "<p>时间到了会自动退出！！需要重新登陆哦！详细问题见[常见问题及回答]</p>");
+        $('#myModal').modal('show');
+        $("#autoOk").bind("click", function ()
+        {
+            isAutoSelectCourse = true;
+            $("#loginPanel").attr("class", "span5");
+            $("#autoSelect").attr("class", "span4");
+            $("#showText").attr("class", " ");
+            $("#showText").hide();
+            $("#autoSelect").fadeIn("slow");
+            
+        }
+        );
+        
+    }
+    );
+    
+    $("#closeForm").bind("click", function () //关闭预选课表
+    {
+        isAutoSelectCourse = false;
+        $("#loginPanel").attr("class", "span9");
+        $("#autoSelect").hide();
+        
+    }
+    );
+    
+    $("#signBtn").bind("click", function () //随机通道和验证码
+    {
+        changeBox();
+    }
+    );
+    
+    
+    $("#closeTips").bind("click", function () //关闭提示
+    {
+        $("#myTips").fadeOut("slow");
+    }
+    );
+    
+    $("#closeMesPanel").bind("click", function () //关闭消息面板
+    {
+        $("#mesPanel").fadeOut("fast");
+    }
+    );
+    
+    $("#loginBtn").bind("click", function () //登陆
+    {
+        document.getElementById("signBtn").disabled = false;
+        checkForm();
+    }
+    );
+    $("#changeCode").bind("click", function () //换验证码
+    {
+        changeImage();
+    }
+    );
+    
+    $("#signImg").keyup(function () //检查验证码是否4位
+    {
+        if (document.getElementById("signImg").value.length == 4)
+            checkForm();
+    }
+    );
+    
+    if (isInSchool)
+    {
+        $("#showXnXwText").html("校内");
+    }
+    else
+    {
+        $("#showXnXwText").html("校外");
+    }
+    $("#showXnXwText").show();
+    
+    readLocalStore(); //读取上次存到本地的个人信息
+}
