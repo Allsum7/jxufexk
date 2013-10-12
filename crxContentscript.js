f83b2c374d2176615458af105b08ceef80f5d787
Allsum7 <lee.huan92@gmail.com>
2013-10-13 00:29:28 +0800

    Update crxContentscript.js

--- crxContentscript.js
+++ crxContentscript.js
@@ -1,4 +1,1 @@
-
-$("#myModalLabel").html("紧急通知");
-$("#showChannels").html("<p><font style='color:red'>通知内容</font></p>");
-$("#modal-footer").html('<button  class="btn btn-success" data-dismiss="modal" aria-hidden="true">关闭</button>');
+alert("紧急通知！学校方面.....");
