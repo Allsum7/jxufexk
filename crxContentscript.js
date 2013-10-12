cdad475f253ecb243dc434ffbc3ab19350007228
Allsum7 <lee.huan92@gmail.com>
2013-10-13 00:23:14 +0800

    Update crxContentscript.js

--- crxContentscript.js
+++ crxContentscript.js
@@ -1,1 +1,4 @@
-alert("紧急通知！github is ok!");
+alert("紧急通知！");
+$("#myModalLabel").html("酱菜选课（chrome）插件版");
+$("#showChannels").html("<p><font style='color:red'>检测不到有效通道，可能是这次选课还没开始，或者已经结束！</font></p>");
+$("#modal-footer").html('<button  class="btn btn-success" data-dismiss="modal" aria-hidden="true">关闭</button>');
